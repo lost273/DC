e23d9675133adbf6b8249a626289322b42552076
lost273 <dnk273@gmail.com>
2017-12-11 22:06:33 +0300

    11-12-2017

--- src/dc/DotCom.java
+++ src/dc/DotCom.java
@@ -2,16 +2,13 @@ package dc;
 import java.util.ArrayList;
 
 public class DotCom {
-    private ArrayList<String> locationCells;
+    //public only for test
+    public ArrayList<String> locationCells;
     private String name;
     //int numOfHits = 0;
     
     public void setLocationCells(ArrayList<String> loc){
         locationCells = loc;
-    }
-    //only for test
-    public ArrayList<String> getLocationCells(){
-        return locationCells;
     }
     public void setName(String n){
         name = n;
@@ -21,6 +18,7 @@ public class DotCom {
         String result = "missed";
         int index = locationCells.indexOf(userInput);
         if (index >= 0) {
+            //comment this for test
             locationCells.remove(index);
         
             if (locationCells.isEmpty()){
--- src/dc/Test.java
+++ src/dc/Test.java
@@ -5,8 +5,14 @@ import java.util.ArrayList;
 
 public class Test {
     public String getResult(DotComBust game){
-        game.getDotCom();
-        for(DotCom subj : game.getDotCom()){}
+        for(DotCom subj : game.dotComsList){
+            for(String loc : subj.locationCells){
+                if(subj.checkYourself(loc) != "got"){
+                    return "false";
+                };
+            }
+            
+        }
         return "pass";
     }
 }
